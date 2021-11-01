7cdc6766c72cea347e389661e7783afa12a8a556
dev global <jaywoc12@gmail.com>
2021-11-01 19:24:54 -0400

    Bat file conversion

--- src/pygitinterface/git_interface.py
+++ src/pygitinterface/git_interface.py
@@ -16,7 +16,8 @@ class GitInterface:
         tag = str(tag)
         if not tag:
             raise Exception('Please enter a file name or a switch')
-        self._create_process(['git', 'add', tag])
+        self._create_bat(f'git add {tag}')
+        self._execute_bat()
 
     def branch(self, src_branch_name, dst_branch_name):
         src_branch_name = str(src_branch_name)
@@ -25,44 +26,59 @@ class GitInterface:
             raise Exception('Please enter a branch name.')
         if not dst_branch_name:
             raise Exception('Please enter a new branch name.')
-        self._create_process(
-            ['git', 'branch', src_branch_name, dst_branch_name])
+        self._create_bat(f'git branch {src_branch_name} {dst_branch_name}')
+        self._execute_bat()
 
     def checkout(self, branch_name):
         branch_name = str(branch_name)
         if not branch_name:
             raise Exception('Please enter a branch name')
-        self._create_process(['git', 'checkout', branch_name])
+        self._create_bat('git checkout{branch_name}')
+        self._execute_bat()
 
     def commit(self, message=None):
         message = str(message)
-        self._create_process(['git', 'commit', '-m', f'{message}'])
+        self._create_bat(f'git commit -m {message}')
+        self._execute_bat()
 
     def pull(self, branch_name):
         if branch_name:
-            self._create_process(['git', 'pull', self.repo_cite, branch_name])
+            self._create_bat('git pull{self.repo_cite} {branch_name}')
+            self._execute_bat()
         else:
-            self._create_process(['git', 'pull'])
+            self._create_bat('git pull')
+            self._execute_bat()
 
     def push(self, branch_name):
         if branch_name:
-            self._create_process(['git', 'push', self.repo_cite, branch_name])
+            self._create_bat(f'git push {self.repo_cite} {branch_name}')
+            self._execute_bat()
         else:
-            self._create_process(['git', 'push'])
+            self._create_bat('git push')
+            self._execute_bat()
 
     def rebase(self, rebase_branch_name):
         rebase_branch_name = str(rebase_branch_name)
         if not rebase_branch_name:
             raise Exception('Please enter a branch name')
-        self._create_process(['git', 'rebase', rebase_branch_name])
+        self._create_bat(f'git rebase {rebase_branch_name}')
+        self._execute_bat()
 
     def status(self):
-        self._create_process(['git', 'status'])
+        self._create_bat('git status')
+        self._execute_bat
 
-    def _create_process(self, args):
-        """Create subprocess from args"""
+    def _create_bat(self, args):
+        """Create subbat from args"""
         if type(args) != list:
             raise Exception('Args must be a list.')
         if not args:
             raise Exception('No args passed.')
-        subprocess.Popen(args, cwd=self.cwd, shell=False)
+        lines = []
+        lines.append(f'cd {self.cwd}')
+        lines.append(f'{args}')
+        with open('commands.bat', 'w+') as file:
+            file.writelines(lines)
+
+    def _execute_bat(self):
+        subprocess.Popen(['commands.bat'], shell=False)
